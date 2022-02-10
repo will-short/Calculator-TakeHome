dc35dffd9565b240c160a58fd4523798de180bfe
will-short <willofmetal85@gmail.com>
2022-02-10 16:16:48 -0600

    added buttons and structure

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  const [calcString, setCalcString] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(e.currentTarget);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <div className="display">
+        <div className="upper">
+          <div className="history"></div>
+          <span className="ans"></span>
+        </div>
+        <div className="lower">
+          <span className="main-display">{calcString}</span>
+        </div>
+      </div>
+      <form className="buttons" onSubmit={handleSubmit}>
+        <div></div>
+        <div></div>
+        <button className="function blue roman">IIVX</button>
+        <button className="function clear">CE</button>
+        <button className="number" value="1">
+          1
+        </button>
+        <button className="number">2</button>
+        <button className="number">3</button>
+        <button className="function">+</button>
+        <button className="number">4</button>
+        <button className="number">5</button>
+        <button className="number">6</button>
+        <button className="function">×</button>
+        <button className="number">7</button>
+        <button className="number">8</button>
+        <button className="number">9</button>
+        <button className="function">-</button>
+        <button className="number">0</button>
+        <button className="number">.</button>
+        <button className="function blue">=</button>
+        <button className="function">/</button>
+      </form>
+    </main>
   );
 }
 
